4ea8cd231c5781cfc44798e6072cdab98a596e12
Valerio Bartoletti <valerio.bartoletti97@gmail.com>
2024-05-28 11:41:00 +0200

    added categories main page (index)

---
+++ net-il-mio-fotoalbum/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using net_il_mio_fotoalbum.Data;
+using net_il_mio_fotoalbum.Models;
+
+namespace net_il_mio_fotoalbum.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ILogger<CategoryController> _logger;
+
+        public CategoryController(ILogger<CategoryController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            List<Category> allCategories = PhotoManager.GetAllCategories();
+            return View(allCategories);
+        }
+    }
+}
